e172c3bdea6c5eb947bfc36fb1b15e0074b5c8ab
pberu8531 <pberu8531@gmail.com>
2026-01-13 11:37:42 +0300

    postgres class setup v4:setup entry index

--- ElasticBeanstalk/src/config/index.ts
+++ ElasticBeanstalk/src/config/index.ts
@@ -2,9 +2,13 @@ import { EnvConfig } from "../types/env.types.js";
 import { logger } from "../utils/logger.js";
 import { PostgresConfig } from "./postgres.config2.js";
 
-function getPostgresPool(env:EnvConfig){
+export async function getPostgresPool(env:EnvConfig){
   try {
+
     const postgres = new PostgresConfig(env)
+    const pgPool = await postgres.connect()
+    return pgPool
+
   } catch (error) {
     throw error;
   }
